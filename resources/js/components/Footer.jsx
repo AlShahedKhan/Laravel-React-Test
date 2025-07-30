c81fe9f409839b80f206007b4e5f0e122a25ffb1
unknown <abdullahalshahedkhan@gmail.com>
2025-07-30 10:30:00 +0600

    polish 2025-07-30

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
     return (
         <footer className='bg-white flex flex-col items-start w-full mx-auto p-6 pb-4 mt-12 text-sm border-t border-gray-200'>
             <div className='flex justify-between flex-col items-start mt-6 md:flex-row md:justify-around md:w-full'>
-                <ul className='flex justify-center items-start gap-5 flex-col text-gray-700'>
+                <ul className='flex justify-center items-start gap-6 flex-col text-gray-700'>
                     <li className='font-semibold text-base'>Links </li>
                     <li><Link to="/about-us" className='hover:border-b-2 border-black'>About us</Link></li>
                     <li><Link to="/cart" className='hover:border-b-2 border-black'>Cart </Link></li>
                     <li><Link to="/wishlist" className='hover:border-b-2 border-black'>Wishlist</Link></li>
                 </ul>
-                <ul className='flex justify-center items-start gap-5 flex-col text-gray-700'>
+                <ul className='flex justify-center items-start gap-6 flex-col text-gray-700'>
                     <li  className='font-semibold text-base'>Sections</li>
                     <li><Link to="/shoes/men" className='hover:border-b-2 border-black'>Men</Link></li>
                     <li><Link to="/shoes/women" className='hover:border-b-2 border-black'>Women </Link></li>
