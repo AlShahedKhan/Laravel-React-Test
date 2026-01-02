28632e39190ebfbd79bb089c205010fb9f525834
unknown <abdullahalshahedkhan@gmail.com>
2026-01-02 10:30:00 +0600

    Update project

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <GenInfo />
       <Brands />
       <div className="md:w-full md:max-w-full xs:mx-2 sm:mx-auto grid gap-8 md:grid-cols-2 md:items-start md:px-3 mt-4">
-        <div className="prose prose-2xl rounded-2xl border border-gray-400 bg-gradient-to-br from-white to-gray-50 p-6 shadow-sm hover:shadow-lg transition-all hover:-translate-y-1">
+        <div className="prose prose-2xl rounded-3xl border border-gray-400 bg-gradient-to-br from-white to-gray-50 p-6 shadow-sm hover:shadow-lg transition-all hover:-translate-y-1">
           <ShopBy title="Best Sellers" filter="bestSellers" />
         </div>
         <div className="prose prose-2xl rounded-2xl border border-gray-400 bg-gradient-to-br from-white to-gray-50 p-6 shadow-sm hover:shadow-lg transition-all hover:-translate-y-1">
