e2ab61a8da692437a9a99794370a1934862e9d26
unknown <abdullahalshahedkhan@gmail.com>
2026-01-08 10:30:00 +0600

    Update project

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -5,7 +5,7 @@ import GenInfo, { Brands } from "../components/GenInfo";
 
 const Home = () => {
   return (
-    <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto py-10 bg-white shadow-lg">
+    <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto py-10 bg-white shadow-xl">
       <Carousel />
       <GenInfo />
       <Brands />
