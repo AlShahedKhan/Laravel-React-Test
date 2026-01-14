35c85a70026a1d220f3d47011fcd6909722de6b3
unknown <abdullahalshahedkhan@gmail.com>
2026-01-14 10:30:00 +0600

    Update project

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Carousel />
       <GenInfo />
       <Brands />
-      <div className="md:w-full md:max-w-full xs:mx-2 sm:mx-auto grid gap-10 md:grid-cols-2 md:items-start md:px-3 mt-4">
+      <div className="md:w-full md:max-w-full xs:mx-2 sm:mx-auto grid gap-10 md:grid-cols-2 md:items-start md:px-4 mt-4">
         <div className="prose prose-2xl rounded-3xl border border-gray-400 bg-gradient-to-br from-white to-gray-50 p-7 shadow-lg hover:shadow-xl transition-all hover:-translate-y-1">
           <ShopBy title="Best Sellers" filter="bestSellers" />
         </div>
