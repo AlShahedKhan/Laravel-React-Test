ff09cd1489b87153ec002a294786f77703b20e50
unknown <abdullahalshahedkhan@gmail.com>
2026-05-04 10:30:00 +0600

    Update project

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -5,15 +5,15 @@ import GenInfo, { Brands } from "../components/GenInfo";
 
 const Home = () => {
   return (
-    <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto py-10 bg-white shadow-2xl">
+    <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto py-11 bg-white shadow-md">
       <Carousel />
       <GenInfo />
       <Brands />
-      <div className="md:w-full md:max-w-full xs:mx-2 sm:mx-auto grid gap-14 md:grid-cols-2 md:items-start md:px-6 mt-8">
-        <div className="prose prose-2xl rounded-3xl border border-gray-500 bg-gradient-to-br from-white to-gray-50 p-8 shadow-lg hover:shadow-2xl transition-all hover:-translate-y-1">
+      <div className="md:w-full md:max-w-full xs:mx-2 sm:mx-auto grid gap-8 md:grid-cols-2 md:items-start md:px-3 mt-4">
+        <div className="prose prose-2xl rounded-2xl border border-gray-300 bg-gradient-to-br from-white to-gray-50 p-9 shadow-xl hover:shadow-md transition-all hover:-translate-y-1">
           <ShopBy title="Best Sellers" filter="bestSellers" />
         </div>
-        <div className="prose prose-2xl rounded-3xl border border-gray-500 bg-gradient-to-br from-white to-gray-50 p-8 shadow-2xl hover:shadow-md transition-all hover:-translate-y-1">
+        <div className="prose prose-2xl rounded-2xl border border-gray-300 bg-gradient-to-br from-white to-gray-50 p-9 shadow-sm hover:shadow-lg transition-all hover:-translate-y-1">
           <ShopBy title="Top Rated" filter="topRated" />
         </div>
       </div>
